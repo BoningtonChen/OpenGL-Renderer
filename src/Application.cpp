b9e3beb19a7339217661febc4e2e70ed91b4e031
Bonington <yikun.chen@163.com>
2022-10-03 11:03:52 +0800

    设置统一变量（uniform vec4）

--- src/Application.cpp
+++ src/Application.cpp
@@ -141,6 +141,7 @@ int main(int argc, char* argv[], char **env)
 
     // ! 启用当前的 Window
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
     // * 初始化 GLEW
     if (glewInit() != GLEW_OK)
@@ -179,6 +180,10 @@ int main(int argc, char* argv[], char **env)
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     GLCall( glUseProgram(shader) );
 
+    GLCall( int location = glGetUniformLocation(shader, "u_Color") );
+    ASSERT(location  != -1);
+    GLCall( glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f) );
+
 #if SHADER_PARSE_STATUS_CHECK
     std::cout << "[VERTEX Shader]:" << std::endl;
     std::cout << source.VertexSource << std::endl;
@@ -186,13 +191,23 @@ int main(int argc, char* argv[], char **env)
     std::cout << source.FragmentSource << std::endl;
 #endif
 
+    float r = 0.0f;
+    float increment = 0.05f;
+
     // ! 循环当前窗口
     while ( !glfwWindowShouldClose(window) )
     {
         // ! 在此处渲染内容
         glClear(GL_COLOR_BUFFER_BIT);
 
+        GLCall( glUniform4f(location, 0.2f, r, 0.8f, 1.0f) );
         GLCall( glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr) );
+
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+            increment = 0.05f;
+        r += increment;
 
         // ! 交换前后端 buffer
         glfwSwapBuffers(window);
@@ -201,7 +216,7 @@ int main(int argc, char* argv[], char **env)
         glfwPollEvents();
     }
 
-    GLCall( glDeleteProgram(shader); );
+    GLCall( glDeleteProgram(shader) );
 
     glfwTerminate();
 
