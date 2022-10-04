655a41647dbc06a092fc067928d704868ecae911
Bonington <yikun.chen@163.com>
2022-10-05 00:10:47 +0800

    投影矩阵简单优化和解析测试

--- src/Application.cpp
+++ src/Application.cpp
@@ -100,15 +100,22 @@ int main(int argc, char* argv[], char **env)
                 0.0f, 540.0f,
                 -1.0f, 1.0f
                 );
+        glm::mat4 view = glm::translate(
+                glm::mat4(1.0f),
+                glm::vec3(-100, 0, 0)
+                );
+        glm::mat4 model = glm::translate(
+                glm::mat4(1.0f),
+                glm::vec3(200, 200, 0)
+                );
 
-        glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
-        glm::vec4 result = projection * vp;
+        glm::mat4 mvp = projection * view * model;
 
         Shader shader("../res/shaders/Basic.shader");
         shader.Bind();
 
         shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
-        shader.SetUniformMat4f("u_MVP", projection);
+        shader.SetUniformMat4f("u_MVP", mvp);
 
         Texture texture("../res/textures/BonityLogo_light.png");
         texture.Bind();
